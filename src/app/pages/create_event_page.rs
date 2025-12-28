f656bdce7e677e4fc78eea1878f75d2f14a25592
wandeltn <niklas.wandelt@gmail.com>
2025-12-28 18:02:53 +0100

    add file upload field

--- src/app/pages/create_event_page.rs
+++ src/app/pages/create_event_page.rs
@@ -5,10 +5,11 @@ use chrono::Local;
 use leptos::leptos_dom::logging::console_log;
 use leptos::prelude::*;
 use leptos::{view, IntoView};
-use phosphor_leptos::{Icon, IconWeight, CALENDAR_DOT, COINS, CURRENCY_EUR, MAP_PIN, TICKET};
+use phosphor_leptos::{Icon, IconWeight, CALENDAR_DOT, COINS, IMAGE, MAP_PIN, TICKET};
 use thaw::{
-    Button, ButtonAppearance, Card, DatePicker, Input, InputSuffix, Label, LabelSize, Select,
-    SpinButton, Textarea, TimePicker,
+    Button, ButtonAppearance, Card, DatePicker, FileList, Input, Label, LabelSize, MessageBar,
+    MessageBarBody, MessageBarIntent, Select, SpinButton, Textarea, TimePicker, Toast, ToastBody,
+    ToasterInjection, Upload, UploadDragger,
 };
 
 #[component]
@@ -23,6 +24,8 @@ pub fn CreateEventPage() -> impl IntoView {
     let ticket_price = RwSignal::new(0.0f64);
     let ticket_quantity = RwSignal::new(0i32);
 
+    let file_uploaded = RwSignal::new(false);
+
     // add_event_api(name, event_date, location, image_base64);
 
     async fn on_submit(event_name: String, event_date: String, location: String) {
@@ -36,6 +39,15 @@ pub fn CreateEventPage() -> impl IntoView {
             Err(e) => console_log(&format!("Failed to create event: {:?}", e)),
         }
     }
+
+    let custom_request = move |file_list: FileList| {
+        let len = file_list.length();
+        file_uploaded.set(true);
+        console_log(&format!(
+            "Custom upload request received with {} files.",
+            len
+        ));
+    };
 
     view! {
         <div class="flex flex-col md:mx-16 mx-4 my-8 gap-8">
@@ -77,12 +89,12 @@ pub fn CreateEventPage() -> impl IntoView {
                     </div>
 
                     <div class="flex md:flex-row flex-col gap-4 grow w-full">
-                        <div class="flex flex-col w-full">
+                        <div class="flex flex-col w-full gap-2">
                             <Label>"Date"</Label>
                             <DatePicker value=event_date />
                         </div>
 
-                        <div class="flex flex-col w-full">
+                        <div class="flex flex-col w-full gap-2">
                             <Label>"Time"</Label>
                             <TimePicker class="grow w-full" value=event_time />
                         </div>
@@ -114,16 +126,17 @@ pub fn CreateEventPage() -> impl IntoView {
                 </div>
 
                 <div class="flex md:flex-row flex-col gap-4 grow w-full">
-                    <div class="flex flex-col w-full">
-                        <Label>"Date"</Label>
+                    <div class="flex flex-col w-full gap-2">
+                        <Label>"Ticket Price (€)"</Label>
                         <SpinButton<f64> value=ticket_price step_page=1f64 placeholder="0.00" />
                     </div>
 
-                    <div class="flex flex-col w-full">
-                        <Label>"Time"</Label>
+                    <div class="flex flex-col w-full gap-2">
+                        <Label>"Ticket Quantity"</Label>
                         <SpinButton<i32> value=ticket_quantity step_page=10 placeholder="100" />
                     </div>
                 </div>
+            </Card>
             // <button
             // type="button"
             // on:click=move |_| spawn_local(
@@ -133,7 +146,27 @@ pub fn CreateEventPage() -> impl IntoView {
                 // >
                 // "Create Event"
                 // </button>
+            <Card class="mt-4">
+                <div class="flex items-center gap-2 mb-2">
+                    <Icon icon=IMAGE size="24px" weight=IconWeight::Regular />
+                    <Label size=LabelSize::Large>"Event Image"</Label>
+                </div>
+
+                <Upload multiple=false accept="image/*" custom_request>
+                    <UploadDragger class="h-32 flex items-center justify-center">
+                        "Click or drag image to this area to upload"
+                    </UploadDragger>
+                </Upload>
+                <Show when=move || {file_uploaded.get()} fallback=|| view! {}>
+                    <MessageBar intent=MessageBarIntent::Success>
+                        <MessageBarBody>
+                            "Image uploaded successfully."
+                        </MessageBarBody>
+                    </MessageBar>
+                </Show>
+
             </Card>
+
             <div class="flex md:flex-row flex-col gap-4 mt-4 justify-center-safe">
                 <Button class="basis-xl max-h-10" appearance=ButtonAppearance::Primary>"Create Event"</Button>
                 <Button class="basis-xs max-h-10" appearance=ButtonAppearance::Secondary>"Cancel"</Button>
